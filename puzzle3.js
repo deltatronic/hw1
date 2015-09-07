4837912aa79a3e800df8a8cd41eaf7e6058bd9c7
deltatronic <ddang@cca.edu>
2015-09-06 20:57:38 -0700

    Update puzzle3.js

--- puzzle3.js
+++ puzzle3.js
@@ -1,35 +1,17 @@
-function goDown(total){
+function goDown(total) {
   var count = 0;
-  while (count < total){
+  while (count < total) {
     down();
     count = count + 1;
   }
 }
- 
- 
-function goRight(total){
+
+function goRight(total) {
   var count = 0;
-  while (count < total){
+  while (count < total) {
     right();
     count = count + 1;
-    }
-}
- 
- 
-function goLeft(total){
-  var count = 0;
-  while (count < total){
-    left();
-    count = count + 1;
-    }
-}
- 
-function goUp(total){
-  var count = 0;
-  while (count < total){
-    up();
-    count = count + 1;
-    }
+  }
 }
 
 goDown(2);
@@ -37,6 +19,7 @@ var oddsquare = getColor();
 getColor();
 goDown(3);
 goRight(2);
+
 setColor(oddsquare);
 goRight(2);
-goUp(1);
+up();
