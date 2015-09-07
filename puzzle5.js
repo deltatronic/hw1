ee55c6c7393ae1fb39b802e5d124bf13aadc8a0b
deltatronic <ddang@cca.edu>
2015-09-06 20:34:06 -0700

    Update puzzle5.js

--- puzzle5.js
+++ puzzle5.js
@@ -1,87 +1,72 @@
-// Help Rudy the Red Dot collect all the green dots!
 
-// Control Rudy with the following commands:
-//  up();                   -- moves Rudy up one square
-//  down();                 -- ...down one square
-//  left();                 -- ...left one square
-//  right();                -- ...right one square
-
-// Interact with Rudy's environment using the following commands:
-//  remainingDots();        -- returns the number of green dots remaining
-//  getColor();             -- returns the color of Rudy's square
-//  setColor(color);        -- changes the color of Rudy's square, opens gates if colors match
-
-
-function iRight(total){
+function goRight(total){
   var count = 0;
   while (count < total){
     right();
-    count++;
+    count = count + 1;
     }
 }
 
 //function key to go left
 
-function iLeft(total){
+function goLeft(total){
   var count = 0;
   while (count < total){
     left();
-    count++;
+    count = count + 1;
     }
 }
 
-//function key to go up
-function iUp(total){
+function goUp(total){
   var count = 0;
   while (count < total){
     up();
-    count++;
+    count = count + 1;
     }
 }
 
-//function key go down 
-function iDown(total){
+function goDown(total){
   var count = 0;
   while (count < total){
     down();
-    count++;
+    count = count + 1;
     }
 }
 
 
 
-iRight(2);
+goRight(2);
 
 if (getColor() == "blue") {
-  iDown(1);
-  iRight(2);
+  goDown(1);
+  goRight(2);
   
 } else {
   
   
-  iUp(1);
-  iRight(2);
+  goUp(1);
+  goRight(2);
 }
 
 if (getColor() == "blue") {
-  iDown(1);
-  iRight(2);
+  goDown(1);
+  goRight(2);
   
 } else {
   
 
   
-  iUp(1);
-  iRight(2);
+  goUp(1);
+  goRight(2);
 }
 
 if (getColor() == "blue") {
-  iDown(1);
-  iRight(3);
+  goDown(1);
+  goRight(3);
   
 } else {
   
   
-  iUp(1);
-  iRight(3);
+  goUp(1);
+  goRight(3);
 }
