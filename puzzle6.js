023f404acfc14fa8d2472f968d174d6ff901f134
deltatronic <ddang@cca.edu>
2015-09-06 20:31:58 -0700

    Update puzzle6.js

--- puzzle6.js
+++ puzzle6.js
@@ -1,23 +1,9 @@
-// Help Rudy the Red Dot collect all the green dots!
 
-// Control Rudy with the following commands:
-//  up();                   -- moves Rudy up one square
-//  down();                 -- ...down one square
-//  left();                 -- ...left one square
-//  right();                -- ...right one square
-
-// Interact with Rudy's environment using the following commands:
-//  remainingDots();        -- returns the number of green dots remaining
-//  getColor();             -- returns the color of Rudy's square
-//  setColor(color);        -- changes the color of Rudy's square, opens gates if colors match
-
-
- 
 function goDown(total){
   var count = 0;
   while (count < total){
     down();
-    count++;
+    count = count + 1;
   }
 }
  
@@ -25,7 +11,7 @@ function goRight(total){
   var count = 0;
   while (count < total){
     right();
-    count++;
+    count = count + 1;
     }
 }
  
@@ -33,7 +19,7 @@ function goLeft(total){
   var count = 0;
   while (count < total){
     left();
-    count++;
+    count = count + 1;
     }
 }
  
@@ -41,7 +27,7 @@ function goUp(total){
   var count = 0;
   while (count < total){
     up();
-    count++;
+    count = count + 1;
     }
 }
  
